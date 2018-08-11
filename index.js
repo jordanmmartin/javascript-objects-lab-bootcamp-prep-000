2e0299f0733e2632caf1951704f78659ba8ae4d6
Jordan Martin <jdordan23@gmail.com>
2018-08-11 18:47:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var recipes = new Object()
+var recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
-  return object
+  object[key] = value;
+  return object;
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
+
+function deleteFromObjectByKey(object, key) {
+   var newObject = Object.assign({}, object);
+   delete newObject.key;
+   return newObject;
+ }
+
